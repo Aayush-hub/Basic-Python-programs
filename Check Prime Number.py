c0282f215f2b9d6cd9a206f1619f098fca5b31e9
Shantanu Taro <43516930+ShantanuTaro@users.noreply.github.com>
2020-10-03 10:20:33 +0530

    Added 3 Basic Python Programs.

---
+++ Check Prime Number.py
@@ -0,0 +1,14 @@
+num = int(input())
+
+if num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+       
+else:
+   print(num,"is not a prime number")
