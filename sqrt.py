34930d7072acf33d6181fc791386f70513cd2cec
Aayush Garg <65889104+Aayush-hub@users.noreply.github.com>
2020-10-03 10:19:17 +0530

    Square root of a number

---
+++ sqrt.py
@@ -0,0 +1,10 @@
+# Python Program to calculate the square root
+
+# Note: change this value for a different result
+num = 8 
+
+# To take the input from the user
+#num = float(input('Enter a number: '))
+
+num_sqrt = round((num ** 0.5),5)
+print(f'The square root of {num} is {num_sqrt}')
