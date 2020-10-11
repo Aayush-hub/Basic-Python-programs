35a8d2153a5d3f13cdd4c3a2bfbc8e309fa70848
Aayush Garg <65889104+Aayush-hub@users.noreply.github.com>
2020-10-11 13:13:30 +0530

    Update quickSortOneLine.py

--- quickSortOneLine.py
+++ quickSortOneLine.py
@@ -1,5 +1,5 @@
 #quick Sort in One Line
 
 quickSort = lambda l : l if len(l)<=1 else qsort([x for x in l[1:] if x < l[0]]) + [l[0]] + qsort([x for x in l[1:] if x >= l[0]])
-
+#driver code
 quickSort([1,2,5,7,8,2,6,8])
