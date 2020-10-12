d8ca2f67638e6c33fef7fb35f874ee5825f7ffe7
Aayush Garg <65889104+Aayush-hub@users.noreply.github.com>
2020-10-12 09:41:30 +0530

    Update QuadraticRoots.py

--- QuadraticRoots.py
+++ QuadraticRoots.py
@@ -14,5 +14,5 @@ d = (b**2) - (4*a*c)
 # find two solutions
 sol1 = (-b-cmath.sqrt(d))/(2*a)
 sol2 = (-b+cmath.sqrt(d))/(2*a)
-
+print("Discrimant is :",d)
 print('The solution are {0} and {1}'.format(sol1,sol2))
